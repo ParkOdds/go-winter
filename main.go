8f38fd1eb0b342fb33e8586b7970dcc0727064df
Park <nattapol@odds.team>
2023-11-12 04:23:35 +0000

    fifth commit

--- main.go
+++ main.go
@@ -4,60 +4,33 @@ import (
 	"fmt"
 )
 
+func GetMember() {
+	fmt.Println("please wait...")
+}
+func IsInSystem(username string) bool {
+	return true
+}
+
+func GetUserDetail(username string) (int, string) {
+	return 201, "manager"
+}
+
+func getDeparture(username string, departure *string) {
+	if username != "" {
+		*departure = "home"
+	}
+}
+
+func CheckLogin(username string, password string) {
+	if IsInSystem(username) {
+		fmt.Println("found user in system")
+		GetUserDetail(username)
+		departure := ""
+		getDeparture(username, &departure)
+	}
+}
+
 func main() {
-	// fmt.Println("enter your name:")
-	// txtInput := ""
-	// // var txtInput string
-	// fmt.Scan(&txtInput)
-	// fmt.Println("" + txtInput)
-
-	// age := 2
-	// fmt.Println(reflect.TypeOf(age))
-
-	// height := "100"
-
-	// // var h int
-	// h, err := strconv.Atoi(height)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-
-	// fmt.Println(h)
-
-	// water := "12.40"
-	// fWater, err := strconv.ParseFloat(water, 32)
-	// fmt.Println(fWater)
-
-	// txtShow := "" // var textShow string = ""
-	// txtShow = fmt.Sprintf("water = %8.2f, height = %d", fWater, h)
-	// fmt.Println(txtShow)
-
-	for i := 0; i < 10; i++ {
-		fmt.Println(i)
-	}
-
-	score := 80
-	switch score {
-	case 80:
-		{
-			fmt.Println("A")
-
-		}
-	case 70:
-		{
-			fmt.Println("B")
-
-		}
-
-	}
-
-	if score > 80 {
-		fmt.Println("A")
-	} else if score < 80 && score > 70 {
-		fmt.Println("B")
-
-	} else {
-		fmt.Println("Unknown")
-	}
+	GetMember()
 
 }
