a72882ab3b53513a4dbd150a60885408c4b81eb2
Park <nattapol@odds.team>
2023-11-12 08:24:26 +0000

    unit test

---
+++ libs/greeting_test.go
@@ -0,0 +1,14 @@
+package libs
+
+import "testing"
+
+func TestLogin(t *testing.T) {
+	Greeting()
+	if Login("admin", "234") != true {
+		t.Error("error test fail")
+	}
+
+	if Login("admin", "234") != false {
+		t.Error("error test pass")
+	}
+}
--- main.go
+++ main.go
@@ -2,35 +2,10 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"myApp/libs"
 )
 
-func Ping1S(c chan int) {
-	for i := 0; i < 10; i++ {
-		fmt.Println("ping : ", i)
-		time.Sleep(1 * time.Second)
-	}
-	c <- 10
-}
-
-func SendNoti5S(ch chan string) {
-	fmt.Println("send noti")
-	time.Sleep(5 * time.Second)
-	fmt.Println("send noti")
-
-	ch <- "success"
-}
-
 func main() {
-
-	c := make(chan int)
-	ch := make(chan string)
-
-	go Ping1S(c)
-	go SendNoti5S(ch)
-
-	pingVal, notMess := <-c, <-ch
-	fmt.Println(pingVal, notMess)
-	// time.Sleep(10 * time.Second)
-	fmt.Println("completed")
+	fmt.Println("Hello")
+	libs.Greeting()
 }
